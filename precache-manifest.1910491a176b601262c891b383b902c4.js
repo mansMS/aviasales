3e3913d4cd4a24a6a1c4c906d9ad4811ede58b90
mansMS <47422885+mansMS@users.noreply.github.com>
2020-01-15 18:25:29 +0300

    Updates

---
+++ precache-manifest.1910491a176b601262c891b383b902c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f587e01a09127f3775ee7ca8029284b0",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "973d1d73de84c4e4c4dc",
+    "url": "/aviasales/static/css/main.ea0c6d78.chunk.css"
+  },
+  {
+    "revision": "5e1ec15c4706342d694c",
+    "url": "/aviasales/static/js/2.6ab90618.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/aviasales/static/js/2.6ab90618.chunk.js.LICENSE"
+  },
+  {
+    "revision": "973d1d73de84c4e4c4dc",
+    "url": "/aviasales/static/js/main.7d42be7a.chunk.js"
+  },
+  {
+    "revision": "e49d50398089f1f7d6c6",
+    "url": "/aviasales/static/js/runtime-main.0df06dac.js"
+  },
+  {
+    "revision": "c5fcf8456b31edcf9d2d565266b78b2d",
+    "url": "/aviasales/static/media/logo.c5fcf845.png"
+  }
+]);
